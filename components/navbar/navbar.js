228726cd11beea45e28f003f54066d4b7dab6d14
banboy38 <nabovtl@gmail.com>
2022-05-29 23:34:25 +0530

    nav right

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -1,7 +1,7 @@
 export default function Navbar(){
     return(
         <>
-            <div className="fixed top-0 left-0 h-[100vh] w-[6rem] flex place-content-center">
+            <div className="fixed top-0 right-0 h-[100vh] w-[6rem] flex place-content-center">
                 <div className='gap-y-2 flex flex-col place-content-center justify-around text-xl '>
                     <a href="#" className="rotate-90 line-through">Home</a>
                     <a href="#" className="rotate-90">About</a>
